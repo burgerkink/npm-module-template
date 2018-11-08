117c981b18a81ff5ea1ee6d13fdf88a13b7e54e4
Martin Swiderski <codemds@8ig.uk>
2018-11-08 11:47:41 +0000

    nvl

--- tests/specs/mocha/nvl-test.js
+++ tests/specs/mocha/nvl-test.js
@@ -1,17 +1,27 @@
 const chai = require('chai');
 const nvl = require('./../../../src/nvl').nvl;
+let udf;
 let assert = chai.assert;
 
-describe('nvl', () => {
+describe('nvl()', () => {
 
-    it('values passed', () => {
+    it('with values passed', () => {
         assert('Hello'.length === 5, 'is 5');
-        assert(typeof nvl({ obj: true}) === 'object', 'is [object]');
+        assert.typeOf(nvl({ obj: true}), 'object', 'is [object]');
         assert(JSON.stringify(nvl({obj: true})) === '{"obj":true}', 'is [string] JSON');
-        assert(typeof nvl(1234) === 'number', 'is [number]');
+        assert.typeOf(nvl(1234), 'number', 'is [number]');
         assert(nvl(1234) === 1234, 'value of: 1234');
     });
 
-    
+    it('with no values', () => {
+        assert.notTypeOf(nvl(udf), 'not undefined');
+        assert(nvl(udf) === null, 'is [null]');
+    });
+
+    it('with no values but with fallback', () => {
+        assert.notTypeOf(nvl(udf, 'fallback'), 'not undefined');
+        assert(nvl(udf, 'fallback') === 'fallback', 'is [string]');
+    });
+
 });
 
