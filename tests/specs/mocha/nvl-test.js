e2883fc7dbfd4f98fd604ecf2375a6414c68720f
Martin Swiderski <codemds@8ig.uk>
2018-11-08 10:54:51 +0000

    nvl

--- tests/specs/mocha/nvl-test.js
+++ tests/specs/mocha/nvl-test.js
@@ -1,9 +1,17 @@
-const chai = require("chai");
+const chai = require('chai');
+const nvl = require('./../../../src/nvl').nvl;
 let assert = chai.assert;
 
-describe('Basic Mocha String Test', () => {
-    it('should return number of charachters in a string', () => {
+describe('nvl', () => {
+
+    it('values passed', () => {
         assert('Hello'.length === 5, 'is 5');
+        assert(typeof nvl({ obj: true}) === 'object', 'is [object]');
+        assert(JSON.stringify(nvl({obj: true})) === '{"obj":true}', 'is [string] JSON');
+        assert(typeof nvl(1234) === 'number', 'is [number]');
+        assert(nvl(1234) === 1234, 'value of: 1234');
     });
+
+    
 });
 
