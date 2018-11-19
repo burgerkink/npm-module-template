dfe2add492707688ea3fb32f3945f53d2d74b856
Martin Swiderski <codemds@8ig.uk>
2018-11-19 09:38:24 +0000

    json-1

---
+++ docs/json-wrap.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/**
+ * Turns k=>v into line of escaped JSON
+ * @param undefined|string k Key
+ * @param undefined|string v Value
+ * @returns {string} Line of JSON
+ */
+function wrapInJSON(k, v) {
+  let key = (typeof k === 'undefined' || k.trim() === '') ? 'err_no_key_defined' : k,
+      val = (typeof v === 'undefined') ? '' : v,
+      arr = JSON.stringify({'replace_this': val}, null, 2).replace('  "replace_this":', '  "'+key+'":').split('\n');
+  return arr[1];
+}
+
+console.log(wrapInJSON(process.argv[2], process.argv[3]));
+process.exit();
